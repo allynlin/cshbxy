3e20b36561f354f58b258d84cb8ce4554f310d6f
allynlin <allynlin@outlook.com>
2022-11-06 14:13:11 +0800

    请假申请国际化
    Signed-off-by: allynlin <allynlin@outlook.com>

--- src/pages/Apply/Leave/index.tsx
+++ src/pages/Apply/Leave/index.tsx
@@ -18,8 +18,8 @@ const LeaveForm = () => {
     const [confirmLoading, setConfirmLoading] = useState<boolean>(false);
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
     const [alertType, setAlertType] = useState<'info' | 'warning' | 'error' | 'success'>('info');
-    const [alertMessage, setAlertMessage] = useState<string>('正在获取提示信息');
-    const [alertDescription, setAlertDescription] = useState<string>('正在请求提示信息，如长期展示此信息，请检查网络连接');
+    const [alertMessage, setAlertMessage] = useState<string>(intl.get('leaveTitle-1'));
+    const [alertDescription, setAlertDescription] = useState<string>(intl.get('leaveDesc-1'));
     // 是否有上一次请假情况
     const [isHaveLastLeave, setIsHaveLastLeave] = useState<boolean>(true);
     // 监听表单数据
@@ -47,30 +47,30 @@ const LeaveForm = () => {
                 if (res.code === 200) {
                     if (res.body.status === 0) {
                         setAlertType('warning')
-                        setAlertMessage('您有未审批通过的请假申请')
-                        setAlertDescription(`您可以在请假记录中查看详情，或咨询上级部门`)
+                        setAlertMessage(intl.get('leaveTitle-2'))
+                        setAlertDescription(intl.get('leaveDesc-2'))
                     } else if (res.body.status === 1) {
                         setAlertType('success')
-                        setAlertMessage('您上一次的请假申请已审批通过')
+                        setAlertMessage(intl.get('leaveTitle-3'))
                         // 判断 res.body.start_time 和现在的时间差，是已经过去了还是还没到
                         const now = moment().format('YYYY-MM-DD HH:mm:ss')
                         const startTime = moment(res.body.start_time).format('YYYY-MM-DD HH:mm:ss')
                         const endTime = moment(res.body.end_time).format('YYYY-MM-DD HH:mm:ss')
                         if (moment(now).isBefore(startTime)) {
-                            setAlertDescription(`您上次提交的请假申请还未到生效时间，如果有变更您可以在请假记录中修改，请注意：修改后的请假申请将重新审批`)
+                            setAlertDescription(intl.get('leaveDesc-3'))
                         } else if (moment(now).isAfter(endTime)) {
-                            setAlertDescription('您上次提交的请假申请已经过期，如果您还未销假请及时销假，如您已销假请忽略此条消息')
+                            setAlertDescription(intl.get('leaveDesc-4'))
                         } else {
-                            setAlertDescription(`您当前正在休假中，不建议再次提交请假申请，如您需要修改请假时间或销假时间，可以在请假记录页面修改，请注意：修改后的请假申请将重新审批。如您已销假请忽略此条消息`)
+                            setAlertDescription(intl.get('leaveDesc-5'))
                         }
                     } else {
                         setAlertType('error')
-                        setAlertMessage('您上一次请假申请已被驳回')
-                        setAlertDescription('如果您再次提交和上一次相同的申请，请先联系上级部门，避免再次被驳回')
+                        setAlertMessage(intl.get('leaveTitle-4'))
+                        setAlertDescription(intl.get('leaveDesc-6'))
                     }
                     setIsHaveLastLeave(true)
                 } else {
-                    setAlertDescription('暂无提示信息')
+                    setAlertDescription(intl.get('leaveDesc-7'))
                     setIsHaveLastLeave(false)
                 }
             }
@@ -85,11 +85,11 @@ const LeaveForm = () => {
             navigate('/home/success', {
                 state: {
                     object: {
-                        title: '请假申请提交成功',
-                        describe: '请等待上级部门审批',
-                        toPage: '查看申请记录',
+                        title: intl.get('leaveApply') + ' ' + intl.get('submitSuccess'),
+                        describe: intl.get('waitApprove'),
+                        toPage: intl.get('showApplyList'),
                         toURL: '/home/record/leave',
-                        againTitle: '继续提交新的申请'
+                        againTitle: intl.get('continueSubmit')
                     }
                 }
             })
@@ -107,7 +107,7 @@ const LeaveForm = () => {
         submitForm();
     };
 
-    const onFinish = (values: any) => {
+    const onFinish = () => {
         setIsModalVisible(true)
     };
 
@@ -148,17 +148,17 @@ const LeaveForm = () => {
         <div className={'apply-body'}>
             {isHaveLastLeave ? <RenderAlert/> : null}
             <Modal
-                title={intl.get('Confirm')}
+                title={intl.get('confirm')}
                 open={isModalVisible}
                 onOk={handleOk}
                 confirmLoading={confirmLoading}
                 onCancel={handleCancel}
             >
-                <p>请假时间：{leaveTime ? leaveTime[0].format('YYYY-MM-DD HH:mm:ss') : '未选择请假时间'}</p>
-                <p>销假时间：{leaveTime ? leaveTime[1].format('YYYY-MM-DD HH:mm:ss') : '未选择销假时间'}</p>
-                <p>请假原因：{reason}</p>
+                <p>{intl.get('startTime')}：{leaveTime ? leaveTime[0].format('YYYY-MM-DD HH:mm:ss') : intl.get('notChooseStartTime')}</p>
+                <p>{intl.get('endTime')}：{leaveTime ? leaveTime[1].format('YYYY-MM-DD HH:mm:ss') : intl.get('notChooseEndTime')}</p>
+                <p>{intl.get('reason')}：{reason}</p>
             </Modal>
-            <Title level={2} className={'tit'}>请假申请</Title>
+            <Title level={2} className={'tit'}>{intl.get('leaveApply')}</Title>
             <Form
                 form={form}
                 name="basic"
@@ -171,9 +171,9 @@ const LeaveForm = () => {
             >
 
                 <Form.Item
-                    label="请假时间"
+                    label={intl.get('leaveTime')}
                     name="leaveTime"
-                    rules={[{required: true, message: '请选择请假时间'}]}
+                    rules={[{required: true, message: intl.get('pleaseChooseLeaveTime')}]}
                 >
                     <DatePicker.RangePicker
                         renderExtraFooter={() =>
@@ -182,33 +182,30 @@ const LeaveForm = () => {
                         showTime={true}
                         format={"YYYY-MM-DD HH:mm:ss"}
                         ranges={{
-                            '快捷选择': [moment(), moment().add(1, 'days')],
-                            '今天': [moment(), moment().endOf('day')],
-                            '一天': [moment(), moment().add(1, 'days')],
-                            '一周': [moment(), moment().add(1, 'weeks')],
-                            '一月': [moment(), moment().add(1, 'months')],
+                            [intl.get('fastChoose')]: [moment(), moment().add(1, 'days')],
+                            [intl.get('today')]: [moment(), moment().endOf('day')],
+                            [intl.get('oneDay')]: [moment(), moment().add(1, 'days')],
+                            [intl.get('oneWeek')]: [moment(), moment().add(1, 'weeks')],
+                            [intl.get('oneMonth')]: [moment(), moment().add(1, 'months')],
                         }}
                     />
                 </Form.Item>
 
                 <Form.Item
-                    label={intl.get('Reason')}
+                    label={intl.get('reason')}
                     name="reason"
-                    rules={[{required: true, message: intl.get('input-required-message', {name: intl.get('Reason')})}]}
+                    rules={[{required: true, message: intl.get('pleaseInputReason')}]}
                 >
-                    <Input.TextArea rows={4} placeholder={intl.get('textarea-enter-placeholder', {
-                        name: intl.get('Reason'),
-                        max: 500
-                    })} showCount={true}
+                    <Input.TextArea rows={4} placeholder={intl.get('pleaseInputReason')} showCount={true}
                                     maxLength={500}/>
                 </Form.Item>
 
                 <Form.Item wrapperCol={{offset: 8, span: 16}} style={{textAlign: "center"}}>
                     <Button type="primary" htmlType="submit">
-                        {intl.get('Submit')}
+                        {intl.get('submit')}
                     </Button>
                     <Button htmlType="button" onClick={onReset} style={{marginLeft: 8}}>
-                        {intl.get('Reset')}
+                        {intl.get('reset')}
                     </Button>
                 </Form.Item>
             </Form>
