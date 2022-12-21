9af1a427fa77fbdde014461534ca934d436afaac
allynlin <allynlin@outlook.com>
2022-12-21 19:35:30 +0800

    保存图片水印优化
    Signed-off-by: allynlin <allynlin@outlook.com>

--- src/component/Tag/RenderWatermarkColor.tsx
+++ src/component/Tag/RenderWatermarkColor.tsx
@@ -9,7 +9,7 @@ export const RenderWatermarkColor = (text: number) => {
 
     switch (text) {
         case 0:
-            return 'rgba(255,141,0,0.3)';
+            return 'rgba(0,0,0,.15)';
         case 1:
             return 'rgba(0,108,1,0.3)';
         case 2:
